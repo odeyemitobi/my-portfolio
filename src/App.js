b0cb903ca838e749ad9a95e453e53c27c0f9b16e
odeyemitobi <105063166+odeyemitobi@users.noreply.github.com>
2023-01-03 16:14:10 +0100

    Navbar update , hover effects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './component/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <NavBar/>
     </div>
   );
 }
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import navIcon1 from '../asset/img/nav-icon1.svg';
+import navIcon2 from '../asset/img/nav-icon2.svg';
+import navIcon3 from '../asset/img/nav-icon3.svg';
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState('home');
@@ -28,7 +31,9 @@ export const NavBar = () => {
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="<Tobi/>"/>
+            <div className="logo-dsg">
+              TOBY
+            </div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -41,9 +46,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={} alt=""/></a>
-                <a href="#"><img src={} alt=""/></a>
-                <a href="#"><img src={} alt=""/></a>
+                <a href="#"><img src={navIcon1} alt=""/></a>
+                <a href="#"><img src={navIcon2} alt=""/></a>
+                <a href="#"><img src={navIcon3} alt=""/></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
